efa9e13b131a12e55e2f868436539c3ef2043ddd
Willow Dennison <dennisonwillow@gmail.com>
2025-10-27 19:29:05 -0500

    Redid evaluator framework, integrated gemini api for evaluator calls. evaluate function takes path to summarizer log, will eventually be directly pipelined in automation process

--- benchmarking.py
+++ benchmarking.py
@@ -24,10 +24,17 @@
 import ollama
 import json
 import re
+from google import genai
+from pydantic import BaseModel
+from dotenv import load_dotenv
 
 
-
-class Agent():
+#intended to do the grunt summarizing work with smaller models
+#prompt will be followed by the article text / other information provided
+#it should describe the information the summarizer will recieve (article text)
+#and what it should return (a summary of the article)
+#as well as a structured output schema
+class Summarizer():
 
 
     def __init__(self, prompt: str, model: str):
@@ -37,9 +44,9 @@ class Agent():
 
     
     #logs the info related to the response provided
-    def log(self, response):
+    def log(self, response, articleText: str):
         
-        logName = 'logs\\' + response.model + '_' + response.created_at + '.json'
+        logName = 'logs\\summary_' + response.model + '_' + response.created_at + '.json'
         logName = re.sub('[<>:"/\\|?*]', '-', logName)
 
         with open(logName, 'w') as f:
@@ -54,70 +61,151 @@ class Agent():
                 "eval_count": response.eval_count,
                 "eval_duration": response.eval_duration,
                 "prompt": self.prompt,
+                "article_text": articleText,
                 "response": response.response,
                 "thinking": response.thinking
             }, f, indent = 4)
-    
+
+            f.close()
 
 
-#intended to use larger models to score responses from summarizer agents
-#prompt will be followed by the text in the functions, 
-#it should describe what information the evaluator is recieving (summary + article text)
-#and how it should be evaluating it (is it an accurate summary?)
-#as well as a structured output schema
-class Evaluator(Agent):
+    #when think == True, there will automatically be a schema added at the end of the prompt 
+    #to account for lack of structured output
+    def summarize(self, articleText: str, think = False):
 
+        if think:
 
-    def __init__(self, prompt: str, model: str):
+            schema = '''JSON schema: {
+                "description": str,
+                "articleSummary": str,
+                "points": list[str],
+                "biasSummary": str
+            }'''
 
-        super().__init__(prompt, model)
+            response = ollama.generate(
+                model = self.model, 
+                prompt = (self.prompt + '\n' + schema + '\n\nArticle text:\n' + articleText),
+                think = True
+            )
 
-    
-    def evaluate(self, inputData: str):
+        else:
 
-        response = ollama.generate(model = self.model, prompt = (self.prompt + '\n\n' + inputData))
+            response = ollama.generate(
+                model = self.model, 
+                prompt = (self.prompt + '\n\nArticle text:\n' + articleText),
+                think = False,
+                format = SumOut.model_json_schema()
+            )
 
-        self.log(response)
-
-        return response
-
-
-
-#intended to do the grunt summarizing work with smaller models
-#prompt will be followed by the article text / other information provided
-#it should describe the information the summarizer will recieve (article text)
-#and what it should return (a summary of the article)
-#as well as a structured output schema
-class Summarizer(Agent):
-
-    
-    def __init__(self, prompt: str, model: str):
-
-        super().__init__(prompt, model)
-
-    
-    def summarize(self, articleText: str):
-
-        response = ollama.generate(model = self.model, prompt = (self.prompt + '\n\n' + articleText))
-
-        self.log(response)
+        self.log(response, articleText)
 
         return response
     
 
 
-if __name__ == '__main__':
+#takes string evalPrompt, the blurb of text that will go before any article summaries, and a gemini model name string
+class Evaluator():
 
-    ollama.pull(model = 'qwen3:8b')
+    
+    def __init__(self, model: str, evalPrompt: str):
+        
+        #loads in api key (GEMINI_API_KEY) from .env file, required for evaluator to function
+        load_dotenv()
 
-    s = Summarizer(
-        prompt = '''Analyze the following article and return a 1 sentence description of its topic, a 1 paragraph narrative summary, a list of 3-5 key points (focusing primarily on factual claims), and a brief summary of the article’s political and reporting biases. 
-        Return only in JSON with the following schema: 
-        {“description”: string, “articlesummary”: string, “points”: list[string], “biassummary”: string}''',
-        model = 'qwen3:8b'
+        self.model = model
+        self.evalPrompt = evalPrompt
+
+    
+    #takes the path to a summarizer output json file and runs the evaluator on that summary, logging the info in its own json
+    def evaluate(self, filePath: str):
+        
+        with open(filePath, 'rb') as f:
+
+            summaryDict = json.load(f)
+
+            f.close()
+
+        client = genai.Client()
+
+        fullPrompt = self.evalPrompt + '\n\nOriginal article text:\n' + summaryDict['article_text'] + '\n\nSummary to be evaluated:\n' + summaryDict['response']['articleSummary'] + '\n\nKey points:\n' + str(summaryDict['response']['points'])
+
+        response = client.models.generate_content(
+            model = self.model,
+            contents = fullPrompt,
+            config = {
+                'response_mime_type': 'application/json',
+                'response_schema': EvalOut
+            }
         )
+
+        return response, summaryDict
     
-    article = '''Article text: WASHINGTON — The Trump administration announced Friday that it has begun "substantial" layoffs of federal workers, as the government remains shut down due to the inability of Congress to reach a funding deal.  "The RIFs have begun," White House budget director Russ Vought said on X, referring to "reduction in force" for workers.  While he didn't provide details, a spokesperson for the White House Office of Management and Budget confirmed to NBC News that the layoffs have begun and said they will be "substantial."  Affected agencies include the departments of Interior, Homeland Security, Treasury, Education, Energy, Housing and Urban Development and Health and Human Services, as well as the Environmental Protection Agency, according to an administration official.  Spokespeople for several of those departments confirmed to NBC News that they were sending layoff notices on Friday but declined to enumerate how many employees were affected, referring comment to OMB.  Democrats pushed back, saying that a shutdown does not require President Donald Trump to fire workers or give him new powers to do so, arguing the White House is being vindictive.  A DHS spokesperson said that the layoffs at the department were occurring within the Cybersecurity and Infrastructure Security Agency, which has been a major target of Trump's since its then-director affirmed that he lost the 2020 election to President Joe Biden. "During the last administration, CISA was focused on censorship, branding and electioneering," the DHS spokesperson said. "This is part of getting CISA back on mission."  HHS spokesman Andrew Nixon said the cuts at that department were focused on countering a "bloated bureaucracy" created under the Biden administration, adding: "HHS continues to close wasteful and duplicative entities, including those that are at odds with the Trump administration’s Make America Healthy Again agenda."  We’d like to hear from you about how you’re experiencing the government shutdown, whether you’re a federal employee who can’t work right now or someone who is feeling the effects of shuttered services in your everyday life. Please contact us at tips@nbcuni.com or reach out to us here.  Prominent unions responded Friday by questioning the legality of the White House’s move and threatening legal action, including AFL-CIO, which tweeted, “America’s unions will see you in court.”  AFSCME President Lee Saunders said the “mass firings are illegal” and will hurt families, vowing to “pursue every available level avenue to stop” the administration’s action.  Federal employee unions had already sued the Trump administration over OMB's threat to trigger mass firings of federal workers before the shutdown even began on Oct. 1. Plaintiffs in that ongoing lawsuit filed a supplementary motion on Friday asking for an immediate temporary restraining order preventing the OMB from ordering agencies to conduct reductions in force. It cited Vought’s post on X declaring that “The RIFs have begun.”  For Trump world, the focus shifts to next year's Nobel Peace Prize  The White House's move defies the wishes of Sen. Susan Collins, R-Maine, the Appropriations Committee chair who oversees government funding.  "I've made very clear that I do not believe there should be firings of furloughed workers," Collins told reporters on Wednesday.  Collins said Friday after the announcement, “I strongly oppose OMB Director Russ Vought’s attempt to permanently lay off federal workers who have been furloughed due to a completely unnecessary government shutdown caused by Senator Schumer.”  Sen. Lisa Murkowski of Alaska, another Republican on the Appropriations Committee, also criticized the layoffs, saying in a post on X that they were "poorly timed and yet another example of this administration’s punitive actions toward the federal workforce."  "The termination of federal employees in a shutdown will further hurt hard-working Americans who have dedicated their lives to public service and jeopardize agency missions once we finally re-open the government," Murkowski wrote.  Senate Minority Leader Chuck Schumer, D-N.Y., said, “Let’s be blunt: nobody’s forcing Trump and Vought to do this. ... They’re callously choosing to hurt people—the workers who protect our country, inspect our food, respond when disasters strike. This is deliberate chaos.”  “Here’s what’s worse: Republicans would rather see thousands of Americans lose their jobs than sit down and negotiate with Democrats to reopen the government,” Schumer said.  And Sen. Patty Murray, D-Wash., the top Democrat on the Appropriations Committee, said “this administration has been recklessly firing—and rehiring—essential workers all year,” adding: “This is nothing new, and no one should be intimidated by these crooks.”  Vought's announcement came one day after the Senate failed for the seventh time to pass either the Republican bill to keep the government open temporarily or the Democratic alternative that includes additional health care funding.'''
+
+    def log(self, response, summaryDict):
+
+        logName = f'logs\\{self.model}_eval_{summaryDict['model']}_{summaryDict['created_at']}.json'
+        logName = re.sub('[<>:"/\\|?*]', '-', logName)
+
+        evaluation = json.loads(response.text)
+
+        with open(logName, 'w') as f:
+            json.dump({
+                'accuracy': evaluation['accuracy'],
+                'completeness': evaluation['completeness'],
+                'reasoning': evaluation['reasoning'],
+                'model': summaryDict['model'],
+                'summaryDict': summaryDict,
+                'evalModel': self.model
+            }, f, indent = 4)
+
+            f.close()
     
-    for i in range(10):
-        s.summarize(article)
+
+
+#custom dict for json schema in summarizer
+class SumOut(BaseModel):
+    description: str
+    articleSummary: str
+    points: list[str]
+    biasSummary: str
+    
+
+
+#custom dict for json schema in evaluator
+class EvalOut(BaseModel):
+    accuracy: int
+    completeness: int
+    reasoning: str
+
+
+
+summaryPrompt = '''You are a tool designed to summarize articles. You will be given the full text of an article, and your task is to return 4 things:
+1. A 1 sentence description of the article for indexing purposes (“description”). This should completely describe the subject of the article without going into too much detail. 
+2. A 2-3 paragraph summary of the article (“articleSummary”). You should aim to cover the content of the article as accurately and completely as possible without editorializing or overexplaining.
+3. A list of 3-5 key points in the article (“points”). These should focus on the factual claims made in the article. Do not comment on the accuracy of the points, only report the direct claims made by the article.
+4. A 2-3 sentence summary of any political and reporting bias apparent from the text of the article (“biasSummary”). 
+
+Output your response in the provided json schema.
+'''
+
+evalPrompt = '''You are a tool designed to rate the accuracy and completeness of article summaries. In this prompt you will be given the full text of an article, a summary of the article, and a list of key factual claims from that article. Your task is to judge the completeness and accuracy of the article and return 3 values:
+1. An accuracy score (“accuracy”) for the summary between 0 and 100 evaluating how accurate the summary and key points are to the original text of the article,
+2. A completeness score (“completeness”) for the summary between 0 and 100 evaluating how complete the summary and key points are and if they left out any important details,
+3. A few sentences justification for the values you chose for accuracy and completeness (“reasoning”).
+
+Output your answer in the provided json schema.
+'''
+
+articleText = '''WASHINGTON — The Trump administration announced Friday that it has begun "substantial" layoffs of federal workers, as the government remains shut down due to the inability of Congress to reach a funding deal.  "The RIFs have begun," White House budget director Russ Vought said on X, referring to "reduction in force" for workers.  While he didn't provide details, a spokesperson for the White House Office of Management and Budget confirmed to NBC News that the layoffs have begun and said they will be "substantial."  Affected agencies include the departments of Interior, Homeland Security, Treasury, Education, Energy, Housing and Urban Development and Health and Human Services, as well as the Environmental Protection Agency, according to an administration official.  Spokespeople for several of those departments confirmed to NBC News that they were sending layoff notices on Friday but declined to enumerate how many employees were affected, referring comment to OMB.  Democrats pushed back, saying that a shutdown does not require President Donald Trump to fire workers or give him new powers to do so, arguing the White House is being vindictive.  A DHS spokesperson said that the layoffs at the department were occurring within the Cybersecurity and Infrastructure Security Agency, which has been a major target of Trump's since its then-director affirmed that he lost the 2020 election to President Joe Biden. "During the last administration, CISA was focused on censorship, branding and electioneering," the DHS spokesperson said. "This is part of getting CISA back on mission."  HHS spokesman Andrew Nixon said the cuts at that department were focused on countering a "bloated bureaucracy" created under the Biden administration, adding: "HHS continues to close wasteful and duplicative entities, including those that are at odds with the Trump administration’s Make America Healthy Again agenda."  We’d like to hear from you about how you’re experiencing the government shutdown, whether you’re a federal employee who can’t work right now or someone who is feeling the effects of shuttered services in your everyday life. Please contact us at tips@nbcuni.com or reach out to us here.  Prominent unions responded Friday by questioning the legality of the White House’s move and threatening legal action, including AFL-CIO, which tweeted, “America’s unions will see you in court.”  AFSCME President Lee Saunders said the “mass firings are illegal” and will hurt families, vowing to “pursue every available level avenue to stop” the administration’s action.  Federal employee unions had already sued the Trump administration over OMB's threat to trigger mass firings of federal workers before the shutdown even began on Oct. 1. Plaintiffs in that ongoing lawsuit filed a supplementary motion on Friday asking for an immediate temporary restraining order preventing the OMB from ordering agencies to conduct reductions in force. It cited Vought’s post on X declaring that “The RIFs have begun.”  For Trump world, the focus shifts to next year's Nobel Peace Prize  The White House's move defies the wishes of Sen. Susan Collins, R-Maine, the Appropriations Committee chair who oversees government funding.  "I've made very clear that I do not believe there should be firings of furloughed workers," Collins told reporters on Wednesday.  Collins said Friday after the announcement, “I strongly oppose OMB Director Russ Vought’s attempt to permanently lay off federal workers who have been furloughed due to a completely unnecessary government shutdown caused by Senator Schumer.”  Sen. Lisa Murkowski of Alaska, another Republican on the Appropriations Committee, also criticized the layoffs, saying in a post on X that they were "poorly timed and yet another example of this administration’s punitive actions toward the federal workforce."  "The termination of federal employees in a shutdown will further hurt hard-working Americans who have dedicated their lives to public service and jeopardize agency missions once we finally re-open the government," Murkowski wrote.  Senate Minority Leader Chuck Schumer, D-N.Y., said, “Let’s be blunt: nobody’s forcing Trump and Vought to do this. ... They’re callously choosing to hurt people—the workers who protect our country, inspect our food, respond when disasters strike. This is deliberate chaos.”  “Here’s what’s worse: Republicans would rather see thousands of Americans lose their jobs than sit down and negotiate with Democrats to reopen the government,” Schumer said.  And Sen. Patty Murray, D-Wash., the top Democrat on the Appropriations Committee, said “this administration has been recklessly firing—and rehiring—essential workers all year,” adding: “This is nothing new, and no one should be intimidated by these crooks.”  Vought's announcement came one day after the Senate failed for the seventh time to pass either the Republican bill to keep the government open temporarily or the Democratic alternative that includes additional health care funding.'''
+summary = '''The article details the Trump administration's initiation of substantial layoffs of federal workers during a government shutdown.\",\n  \"articlesummary\": \"Amid a government shutdown due to a funding impasse, the Trump administration began laying off federal workers, a move condemned by Democrats and some Republicans as vindictive and illegal. Affected agencies span multiple departments, including Homeland Security, Treasury, and Health and Human Services, with justifications provided by the administration centered around eliminating bureaucracy and refocusing agency missions. Unions have threatened legal action, and several senators have voiced opposition to the layoffs, highlighting the political and legal challenges surrounding the administration's actions.'''
+
+e = Evaluator('gemini-2.5-pro', evalPrompt)
+response, summaryDict = e.evaluate('C:\\Users\\Willow (Display)\\Documents\\Programming Projects\\School\\news-fact-checker\\logs\\qwen3-8b_2025-10-27T21-01-56.3890504Z.json')
+e.log(response, summaryDict)
+
+
+# print(response)
+
+# s = Summarizer(summaryPrompt, 'qwen3:8b')
+
+# s.log(s.summarize(articleText, think = False), articleText)
