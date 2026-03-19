c2ba3e7d4f113c7dd1817f22b9440c990d9e4e48
Willow Dennison <dennisonwillow@gmail.com>
2026-03-19 15:37:37 -0500

    all but final step of article fact checking pipeline. Also error handling for json formatting in ollama output.

--- benchmarking/benchmarker.py
+++ benchmarking/benchmarker.py
@@ -117,7 +117,7 @@ class EvalOut(BaseModel):
 summarizer_prompt = '''You are a tool designed to summarize articles. You will be given the full text of an article, and your task is to return 4 things:
 A 1 sentence description of the article for indexing purposes (“description”). This should completely describe the subject of the article without going into too much detail.
 A 2-3 paragraph summary of the article (“articleSummary”). You should aim to cover the content of the article as accurately and completely as possible without editorializing or overexplaining.
-A list of 3-5 key points in the article (“points”). These should focus on the factual claims made in the article. Do not comment on the accuracy of the points, only report the direct claims made by the article.
+A list of 2-3 key points in the article (“points”). These should focus on the factual claims made in the article. Do not comment on the accuracy of the points, only report the direct claims made by the article.
 A 2-3 sentence summary of any political and reporting bias apparent from the text of the article (“biasSummary”).
 
 Output your response in the provided json schema.
