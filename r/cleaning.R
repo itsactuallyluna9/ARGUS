e1c7b83f0d31b589e19038e51b3e24211c61fdc0
Willow Dennison <dennisonwillow@gmail.com>
2026-03-30 15:15:05 -0500

    r

--- r/cleaning.R
+++ r/cleaning.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(rjson)
 
 article_data <- fromJSON(file=file.path(getwd(), "article_data.json"))
-fact_check_data <- fromJSON(file.path(getwd(), "data", "fact_check_data.json"))
+fact_check_data <- fromJSON(file=file.path(getwd(), "fact_check_data.json"))
 
-a
+View(article_data)
+View(fact_check_data)
