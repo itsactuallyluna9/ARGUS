69cd84aed3b8cfaaf9d4e09157effd12624a549e
Willow Dennison <dennisonwillow@gmail.com>
2026-04-03 10:45:19 -0500

    did the python solution

--- src/argus/llamarouter.py
+++ src/argus/llamarouter.py
@@ -63,7 +63,7 @@ class LlamaRouter:
         length = len(prompt) + (len(str(format)) if format else 0) + (1000 if think else 0)
         tokens = length / 4 + 100
 
-        routes = list(filter(lambda route: route.max_tokens >= tokens))
+        routes = [route for route in routes if route.max_tokens >= tokens]
 
         if not routes:
             raise ValueError(f"No routes available for model {model} that can handle the prompt length")
@@ -166,7 +166,7 @@ class LlamaRouter:
         length = len("".join(message["content"] for message in messages if "content" in message)) + len("".join(message["role"] for message in messages if "role" in message)) + (len(str(format)) if format else 0) + (len(str(tools)) if tools else 0) + (1000 if think else 0)
         tokens = length / 4 + 100
 
-        routes = list(filter(lambda route: route.max_tokens >= tokens))
+        routes = [route for route in routes if route.max_tokens >= tokens]
 
         if not routes:
             raise ValueError(f"No routes available for model {model} that can handle the prompt length")
